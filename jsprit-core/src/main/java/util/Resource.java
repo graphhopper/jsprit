8711c45f7416c609a12447155665baa0b69d6585
Stefan Schroeder <4sschroeder@gmail.com>
2013-06-06 09:47:51 +0200

    add header to resource

--- jsprit-core/src/main/java/util/Resource.java
+++ jsprit-core/src/main/java/util/Resource.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Stefan Schroeder.
+ * eMail: stefan.schroeder@kit.edu
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Stefan Schroeder - initial API and implementation
+ ******************************************************************************/
 package util;
 
 import java.io.File;
