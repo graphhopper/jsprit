5b0857d6bff599214a852d1369c199264eb66e8b
oblonski <4sschroeder@gmail.com>
2013-08-13 20:55:17 +0200

    add javaDoc to RuinStrategy

--- jsprit-core/src/main/java/algorithms/RuinStrategy.java
+++ jsprit-core/src/main/java/algorithms/RuinStrategy.java
@@ -28,27 +28,58 @@ import basics.route.VehicleRoute;
 
 interface RuinStrategy {
 	
+	/**
+	 * Listener that listens to the ruin-process. It informs whoever is interested about start, end and about a removal of a job.
+	 * 
+	 * @author schroeder
+	 *
+	 */
 	public static interface RuinListener {
 		
+		/**
+		 * informs about ruin-start.
+		 * 
+		 * @param routes
+		 */
 		public void ruinStarts(Collection<VehicleRoute> routes);
 		
+		/**
+		 * informs about ruin-end.
+		 * 
+		 * @param routes
+		 * @param unassignedJobs
+		 */
 		public void ruinEnds(Collection<VehicleRoute> routes, Collection<Job> unassignedJobs);
 		
+		/**
+		 * informs if a {@link Job} has been removed from a {@link VehicleRoute}.
+		 * 
+		 * @param job
+		 * @param fromRoute
+		 */
 		public void removed(Job job, VehicleRoute fromRoute);
 		
 	}
 
 	/**
-	 * Ruins a current solution, i.e. removes jobs from service providers and
-	 * returns a collection of these removed, and thus unassigned, jobs.
+	 * Ruins a current solution, i.e. a collection of vehicle-routes and 
+	 * returns a collection of removed and thus unassigned jobs.
 	 * 
-	 * @param vehicleRoutes
-	 * @return
+	 * @param {@link VehicleRoute}
+	 * @return Collection of {@link Job}
 	 */
 	public Collection<Job> ruin(Collection<VehicleRoute> vehicleRoutes);
 
+	/**
+	 * Removes targetJob as well as its neighbors with a size of (nOfJobs2BeRemoved-1). 
+	 */
 	public Collection<Job> ruin(Collection<VehicleRoute> vehicleRoutes, Job targetJob, int nOfJobs2BeRemoved);
 	
+	/**
+	 * Adds a ruin-listener.
+	 * 
+	 * @param {@link RuinListener}
+	 */
 	public void addListener(RuinListener ruinListener);
 
 }
