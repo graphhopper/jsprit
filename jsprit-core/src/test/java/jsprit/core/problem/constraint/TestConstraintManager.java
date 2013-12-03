4e38586b50403ee56bbf10ee441618dd9b86ba0a
oblonski <4sschroeder@gmail.com>
2013-12-03 07:55:44 +0100

    test ConstraintManager

---
+++ jsprit-core/src/test/java/jsprit/core/problem/constraint/TestConstraintManager.java
@@ -0,0 +1,14 @@
+package jsprit.core.problem.constraint;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestConstraintManager {
+
+	@Test
+	public void testIt(){
+		assertFalse(true);
+	}
+
+}
