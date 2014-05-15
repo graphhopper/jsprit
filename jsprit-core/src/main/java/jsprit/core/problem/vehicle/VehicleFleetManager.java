1a67d1e20e7e4979a56be96a2758ac9d41338030
oblonski <4sschroeder@gmail.com>
2014-05-15 10:48:12 +0200

    added javadoc to core.problem.vehicle.VehicleFleetManager

--- jsprit-core/src/main/java/jsprit/core/problem/vehicle/VehicleFleetManager.java
+++ jsprit-core/src/main/java/jsprit/core/problem/vehicle/VehicleFleetManager.java
@@ -21,14 +21,46 @@ import java.util.Collection;
 
 public interface VehicleFleetManager {
 	
+	/**
+	 * Locks vehicle.
+	 * 
+	 * <p>This indicates that this vehicle is being used. Thus it is not in list of available vehicles.
+	 * @param vehicle
+	 */
 	public abstract void lock(Vehicle vehicle);
 
+	/**
+	 * Unlocks vehicle.
+	 * 
+	 * <p>This indicates that this vehicle is not being used anymore. Thus it is in list of available vehicles.
+	 * @param vehicle
+	 */
 	public abstract void unlock(Vehicle vehicle);
 
+	/**
+	 * Returns true if locked.
+	 * 
+	 * @param vehicle
+	 * @return
+	 */
 	public abstract boolean isLocked(Vehicle vehicle);
 
+	/**
+	 * Unlocks all locked vehicles.
+	 * 
+	 */
 	public abstract void unlockAll();
 
+	/**
+	 * Returns a collection of available vehicles.
+	 * 
+	 * <p>Note that this does not return ALL available vehicles that were added to the fleetmanager. Vehicles are clustered according
+	 * to {@link VehicleTypeKey}. If there are two unlocked vehicle with the same VehicleTypeKey then only one of them will be returned.
+	 * This is to avoid returning too many vehicles that are basically equal.
+	 * <p>Look at {@link VehicleTypeKey} to figure out whether two vehicles are equal or not.
+	 * 
+	 * @return
+	 */
 	public abstract Collection<Vehicle> getAvailableVehicles();
 
 	/**
