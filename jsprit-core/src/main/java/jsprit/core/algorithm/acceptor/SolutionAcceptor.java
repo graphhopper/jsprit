1540f35cbb5dffca72c1d72eb753e8fc66558bfd
oblonski <4sschroeder@gmail.com>
2014-08-26 19:37:29 +0200

    replace old GPL header with new LGPL header (#126)

--- jsprit-core/src/main/java/jsprit/core/algorithm/acceptor/SolutionAcceptor.java
+++ jsprit-core/src/main/java/jsprit/core/algorithm/acceptor/SolutionAcceptor.java
@@ -1,24 +1,26 @@
+
 /*******************************************************************************
- * Copyright (C) 2013  Stefan Schroeder
- * 
+ * Copyright (C) 2014  Stefan Schroeder
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either 
+ * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public 
+ *
+ * You should have received a copy of the GNU Lesser General Public
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
+
 package jsprit.core.algorithm.acceptor;
 
-import java.util.Collection;
-
 import jsprit.core.problem.solution.VehicleRoutingProblemSolution;
+
+import java.util.Collection;
 
 
 /**
@@ -35,8 +37,8 @@ public interface SolutionAcceptor {
 	 * 
 	 * <p>If the solution is accepted, it is added to solutions, i.e. the solutions-collections is modified.
 	 * 
-	 * @param solutions
-	 * @param newSolution
+	 * @param solutions collection of existing solutions
+	 * @param newSolution new solution to be evaluated
 	 * @return TODO
 	 */
 	public boolean acceptSolution(Collection<VehicleRoutingProblemSolution> solutions, VehicleRoutingProblemSolution newSolution);
