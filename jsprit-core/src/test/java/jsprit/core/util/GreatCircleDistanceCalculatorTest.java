5d0a962e2426c9902130265ccc2d8852cab7aebc
oblonski <4sschroeder@gmail.com>
2014-11-28 10:30:06 +0100

    add great circle distance calc

--- jsprit-core/src/test/java/jsprit/core/util/GreatCircleDistanceCalculatorTest.java
+++ jsprit-core/src/test/java/jsprit/core/util/GreatCircleDistanceCalculatorTest.java
@@ -11,18 +11,34 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public 
+ * You should have received a copy of the GNU Lesser General Public
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
+
 package jsprit.core.util;
 
+import junit.framework.Assert;
+import org.junit.Test;
 
+/**
+ * Created by schroeder on 28.11.14.
+ */
+public class GreatCircleDistanceCalculatorTest {
 
-public class ManhattanDistanceCalculator {
+    @Test
+    public void test(){
+        double lon1 = 8.3858333;
+        double lat1 = 49.0047222;
 
-	public static double calculateDistance(Coordinate coord1, Coordinate coord2) {
-		return Math.abs(coord1.getX() - coord2.getX())
-				+ Math.abs(coord1.getY() - coord2.getY());
-	}
+        double lon2 = 12.1333333;
+        double lat2 = 54.0833333;
+
+        double greatCircle = GreatCircleDistanceCalculator.calculateDistance(
+                Coordinate.newInstance(lon1,lat1),
+                Coordinate.newInstance(lon2,lat2)
+        );
+        Assert.assertEquals(600,greatCircle,30.);
+    }
+
 
 }
