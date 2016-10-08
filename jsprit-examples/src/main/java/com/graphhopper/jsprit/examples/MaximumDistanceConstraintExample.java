7b66c23d8d2600fc3b0befbd57e428f17bee7961
Bhoumik Shah <bhoumiks5910@gmail.com>
2016-10-08 10:49:43 +0530

    Add functionality to consider shipments in maximum distance constraint

--- jsprit-examples/src/main/java/com/graphhopper/jsprit/examples/MaximumDistanceConstraintExample.java
+++ jsprit-examples/src/main/java/com/graphhopper/jsprit/examples/MaximumDistanceConstraintExample.java
@@ -25,15 +25,14 @@ import com.graphhopper.jsprit.core.algorithm.box.Jsprit;
 import com.graphhopper.jsprit.core.algorithm.state.StateId;
 import com.graphhopper.jsprit.core.algorithm.state.StateManager;
 import com.graphhopper.jsprit.core.algorithm.state.StateUpdater;
+import com.graphhopper.jsprit.core.problem.Location;
 import com.graphhopper.jsprit.core.problem.VehicleRoutingProblem;
 import com.graphhopper.jsprit.core.problem.constraint.ConstraintManager;
 import com.graphhopper.jsprit.core.problem.constraint.HardActivityConstraint;
 import com.graphhopper.jsprit.core.problem.misc.JobInsertionContext;
 import com.graphhopper.jsprit.core.problem.solution.VehicleRoutingProblemSolution;
 import com.graphhopper.jsprit.core.problem.solution.route.VehicleRoute;
-import com.graphhopper.jsprit.core.problem.solution.route.activity.ActivityVisitor;
-import com.graphhopper.jsprit.core.problem.solution.route.activity.TourActivity;
-import com.graphhopper.jsprit.core.problem.solution.route.activity.vehicleDependentActivityVisitor;
+import com.graphhopper.jsprit.core.problem.solution.route.activity.*;
 import com.graphhopper.jsprit.core.problem.vehicle.Vehicle;
 import com.graphhopper.jsprit.core.reporting.SolutionPrinter;
 import com.graphhopper.jsprit.core.util.Coordinate;
@@ -52,10 +51,9 @@ public class MaximumDistanceConstraintExample {
 
         private final StateManager stateManager;
 
-        private final VehicleRoutingTransportCostsMatrix costMatrix;
+        private final VehicleRoutingTransportCostsMatrix costsMatrix;
 
-        //        private final StateFactory.StateId distanceStateId;    //v1.3.1
-        private final StateId distanceStateId; //head of development - upcoming release
+        private final StateId distanceStateId;
 
         private VehicleRoute vehicleRoute;
 
@@ -63,40 +61,39 @@ public class MaximumDistanceConstraintExample {
 
         private double distance = 0.;
 
-        private TourActivity prevAct;
+        private Location prevLoc;
 
-        //        public DistanceUpdater(StateFactory.StateId distanceStateId, StateManager stateManager, VehicleRoutingTransportCostsMatrix costMatrix) { //v1.3.1
-        public DistanceUpdater(StateId distanceStateId, StateManager stateManager, VehicleRoutingTransportCostsMatrix transportCosts) { //head of development - upcoming release (v1.4)
-            this.costMatrix = transportCosts;
+        public DistanceUpdater(StateId distanceStateId, StateManager stateManager, VehicleRoutingTransportCostsMatrix transportCosts) {
+            this.costsMatrix = transportCosts;
             this.stateManager = stateManager;
             this.distanceStateId = distanceStateId;
         }
 
-        @Override
         public void begin(VehicleRoute vehicleRoute, Vehicle vehicle) {
             distance = 0.;
-            prevAct = vehicleRoute.getStart();
+            prevLoc = vehicle.getStartLocation();
             this.vehicleRoute = vehicleRoute;
             this.vehicle = vehicle;
         }
 
-        @Override
+
         public void visit(TourActivity tourActivity) {
-            distance += getDistance(prevAct, tourActivity);
-            prevAct = tourActivity;
+            distance += getDistance(prevLoc, tourActivity.getLocation());
+            prevLoc = tourActivity.getLocation();
         }
 
-        @Override
+
         public void finish() {
-            distance += getDistance(prevAct, vehicleRoute.getEnd());
-//            stateManager.putTypedRouteState(vehicleRoute,distanceStateId,Double.class,distance); //v1.3.1
-            stateManager.putRouteState(vehicleRoute,vehicle, distanceStateId, distance); //head of development - upcoming release (v1.4)
+            if(vehicle.isReturnToDepot())
+                distance += getDistance(prevLoc, vehicle.getEndLocation());
+            stateManager.putRouteState(vehicleRoute,vehicle,distanceStateId, distance);
         }
 
-        double getDistance(TourActivity from, TourActivity to) {
-            return costMatrix.getDistance(from.getLocation().getId(), to.getLocation().getId());
+        double getDistance(Location from, Location to) {
+            return this.costsMatrix.getDistance(from.getId(), to.getId());
         }
     }
+
 
     static class DistanceConstraint implements HardActivityConstraint {
 
@@ -106,30 +103,96 @@ public class MaximumDistanceConstraintExample {
 
         private final double maxDistance;
 
-        //        private final StateFactory.StateId distanceStateId; //v1.3.1
-        private final StateId distanceStateId; //head of development - upcoming release (v1.4)
+        private final StateId distanceStateId;
 
-        //        DistanceConstraint(double maxDistance, StateFactory.StateId distanceStateId, StateManager stateManager, VehicleRoutingTransportCostsMatrix costsMatrix) { //v1.3.1
-        DistanceConstraint(double maxDistance, StateId distanceStateId, StateManager stateManager, VehicleRoutingTransportCostsMatrix transportCosts) { //head of development - upcoming release (v1.4)
+
+        DistanceConstraint(double maxDistance, StateId distanceStateId, StateManager stateManager, VehicleRoutingTransportCostsMatrix transportCosts) {
             this.costsMatrix = transportCosts;
             this.maxDistance = maxDistance;
             this.stateManager = stateManager;
             this.distanceStateId = distanceStateId;
         }
 
-        @Override
+
+
         public ConstraintsStatus fulfilled(JobInsertionContext context, TourActivity prevAct, TourActivity newAct, TourActivity nextAct, double v) {
-            double additionalDistance = getDistance(prevAct, newAct) + getDistance(newAct, nextAct) - getDistance(prevAct, nextAct);
-            Double routeDistance = stateManager.getRouteState(context.getRoute(), distanceStateId, Double.class);
-            if (routeDistance == null) routeDistance = 0.;
-            double newRouteDistance = routeDistance + additionalDistance;
-            if (newRouteDistance > maxDistance) {
-                return ConstraintsStatus.NOT_FULFILLED;
-            } else return ConstraintsStatus.FULFILLED;
+            double additionalDistance = 0.;
+            double pickupDistance = 0.;
+
+            if(newAct instanceof DeliverShipment) {
+
+                Location pickupLoc = context.getAssociatedActivities().get(0).getLocation();
+                Location prevtoPickupLoc = null;
+                Location nexttoPickupLoc = null;
+                int pickupActinsertionIndex = context.getRelatedActivityContext().getInsertionIndex();
+
+                /*
+                If first delivery activity
+                Total distance = Start-pickup + pickup-delivery + delivery-end
+                 */
+                if(context.getRoute().getTourActivities().getActivities().size() == 0){
+                    Double routeDistance = this.costsMatrix.getDistance(context.getNewVehicle().getStartLocation().getId(),pickupLoc.getId())
+                                            + getDistance(pickupLoc,newAct.getLocation());
+                    if(context.getNewVehicle().isReturnToDepot())
+                        routeDistance += getDistance(newAct.getLocation(),context.getNewVehicle().getEndLocation());
+                    if(routeDistance > this.maxDistance )return ConstraintsStatus.NOT_FULFILLED;
+                    else  return ConstraintsStatus.FULFILLED;
+
+                /*
+                    If pickup insertion index == 0
+                    Activity previous to pickup is vehicle start
+                     */
+                }else if (pickupActinsertionIndex == 0) {
+                    prevtoPickupLoc = context.getNewVehicle().getStartLocation();
+                    nexttoPickupLoc = context.getRoute().getTourActivities().getActivities().get(0).getLocation();
+                    /*
+                    If pickup activity is just before end activity
+                    next to pickup activity is vehicle end
+                     */
+                } else if (pickupActinsertionIndex == context.getRoute().getTourActivities().getActivities().size()) {
+                    prevtoPickupLoc = context.getRoute().getTourActivities().getActivities().get(pickupActinsertionIndex - 1).getLocation();
+                    nexttoPickupLoc = context.getNewVehicle().getEndLocation();
+                } else {
+                    prevtoPickupLoc = context.getRoute().getTourActivities().getActivities().get(pickupActinsertionIndex - 1).getLocation();
+                    nexttoPickupLoc = context.getRoute().getTourActivities().getActivities().get(pickupActinsertionIndex).getLocation();
+                }
+                /*
+                If next to pickup is end and !vehicle.returntoDepot()
+                Pickup distance = distance(prevtoPickup,Pickup)
+                 */
+                if (pickupActinsertionIndex == context.getRoute().getTourActivities().getActivities().size()  && !context.getNewVehicle().isReturnToDepot())
+                    pickupDistance = getDistance(prevtoPickupLoc, pickupLoc);
+                else
+                    pickupDistance = getDistance(prevtoPickupLoc, pickupLoc) + getDistance(pickupLoc, nexttoPickupLoc) - getDistance(prevtoPickupLoc, nexttoPickupLoc);
+            }
+
+
+            if (nextAct instanceof End && !context.getNewVehicle().isReturnToDepot())
+                additionalDistance = getDistance(prevAct.getLocation(), newAct.getLocation());
+            else if (nextAct instanceof End)
+                additionalDistance =  getDistance(prevAct.getLocation(), newAct.getLocation())
+                                    + getDistance(newAct.getLocation(),context.getNewVehicle().getEndLocation())
+                                    - getDistance(prevAct.getLocation(),context.getNewVehicle().getEndLocation());
+            else if(prevAct instanceof Start)
+                additionalDistance = getDistance(context.getNewVehicle().getStartLocation(), newAct.getLocation())
+                                    + getDistance(newAct.getLocation(),newAct.getLocation())
+                                    - getDistance(context.getNewVehicle().getStartLocation(), nextAct.getLocation());
+            else
+                additionalDistance = getDistance(prevAct.getLocation(), newAct.getLocation())
+                                    + getDistance(newAct.getLocation(), nextAct.getLocation())
+                                    - getDistance(prevAct.getLocation(), nextAct.getLocation());
+
+            Double routeDistance = stateManager.getRouteState(context.getRoute(), context.getNewVehicle(), distanceStateId, Double.class);
+            if (routeDistance == null) {
+                routeDistance = 0.;
+            }
+            double newRouteDistance = routeDistance + additionalDistance + pickupDistance;
+            if (newRouteDistance > maxDistance) return ConstraintsStatus.NOT_FULFILLED;
+            else return ConstraintsStatus.FULFILLED;
         }
 
-        double getDistance(TourActivity from, TourActivity to) {
-            return costsMatrix.getDistance(from.getLocation().getId(), to.getLocation().getId());
+        double getDistance(Location from, Location to) {
+            return this.costsMatrix.getDistance(from.getId(), to.getId());
         }
 
     }
